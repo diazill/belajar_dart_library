7a8ec6671da4ec0d2687bddb73483856bb46a064
Aditya Diaz Illyasa <diazillyasa999@gmail.com>
2025-08-10 10:28:03 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main(List<String> args) {
+  print(sayHello('adit'));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
